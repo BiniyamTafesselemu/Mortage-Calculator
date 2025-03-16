1f993b1965fc7edaa04bc63fcbff6012afd97e03
BiniyamTafesse <biniyamtafesse228@gmail.com>
2025-03-16 16:28:04 +0300

    right side insert

--- src/Mortage.tsx
+++ src/Mortage.tsx
@@ -1,12 +1,15 @@
+import calculatorempty from './assets/illustration-empty.svg';
+import calculator from './assets/icon-calculator.svg';
+
 const Mortage = () => {
   return (
     <div className="flex flex-col items-center min-h-screen p-5 justify-center">
-      <div className="rounded-2xl w-full max-w-[800px] shadow-lg bg-white flex flex-row">
+      <div className="rounded-2xl w-full max-w-[1000px] shadow-lg bg-white flex flex-row">
         {/* Left Side */}
         <div className="flex-1 flex flex-col justify-start m-9">
           <div className="flex justify-between mb-2">
-            <h1 className="text-2xl"><b>Mortgage Calculator</b></h1>
-            <p className="cursor-pointer underline">Clear All</p>
+            <h1 className="text-2xl">Mortgage Calculator</h1>
+            <p className="cursor-pointer mt-1 underline">Clear All</p>
           </div>
           
           {/* Form for Mortgage Details */}
@@ -63,13 +66,13 @@ const Mortage = () => {
             <div className="mb-4">
               <label className="block mb-1">Mortgage Type:</label>
               <div className="flex flex-col">
-                <div className="border border-smoke rounded mb-2">
+                <div className="border border-smoke hover:border-lime active:bg-lime-100 rounded mb-2">
                   <label className="flex items-center p-2">
                     <input type="radio" name="mortgageType" value="repayment" />
                     <span className="ml-2">Repayment</span>
                   </label>
                 </div>
-                <div className="border border-smoke rounded">
+                <div className="border border-smoke hover:border-lime rounded">
                   <label className="flex items-center p-2">
                     <input type="radio" name="mortgageType" value="interest-only" />
                     <span className="ml-2">Interest Only</span>
@@ -79,21 +82,23 @@ const Mortage = () => {
             </div>
 
             {/* Calculate Button */}
-            <button type="submit" className="bg-blue-500 text-white rounded p-2">Calculate Repayments</button>
+            <button type="submit" className="rounded-3xl flex flex-row justify-center items-center bg-lime w-[250px] text-white "><img src={calculator} /><b className="text-black m-2">Calculate Repayments</b></button>
           </form>
         </div>
         
         {/* Right Side */}
-        <div className="flex-1 rounded-tr-2xl rounded-br-2xl rounded-tl-1xl rounded-bl-4xl bg-darkblue p-5">
-          <div className="ml-5 mt-5">
-            <h1 className="text-white">Right Side Content</h1>
-            <h1 className="text-white">Right Side Content</h1>
-            <h1 className="text-white">Right Side Content</h1>
-            <h1 className="text-white">Right Side Content</h1>
-            <h1 className="text-white">Right Side Content</h1>
-            <h1 className="text-white">Right Side Content</h1>
+        <div className="flex-1 rounded-tr-2xl rounded-br-2xl rounded-tl-1xl rounded-bl-[80px] bg-darkblue p-5">
+          <div className="flex flex-col justify-center items-center h-full">
+            <img src={calculatorempty} alt='calculator' className="mx-auto" />
+            <h1 className='text-white text-2xl'>Results shown here</h1>
+            <p className='text-gray-400 mt-3 text-center'>
+              complete the form and click "calculate repayments" to <br />
+              <span className="block text-center">see what your monthly repayments would be.</span>
+            </p>
           </div>
         </div>
+
+
       </div>
     </div>
   );
---
+++ src/assets/assets.js
@@ -0,0 +1,7 @@
+import calculatorempty from './illustration-empty.svg'
+import calculator from './icon-calculator.svg'
+
+export default  assets={
+    calculatorempty,
+    calculator,
+}
